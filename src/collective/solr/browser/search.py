236dce66e9d7cc3428bfac0a20b3d8e008e5d881
Timo Stollenwerk <contact@timostollenwerk.net>
2014-09-01 17:48:28 +0200

    Move catalog query into view.

--- src/collective/solr/browser/search.py
+++ src/collective/solr/browser/search.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.solr.browser.facets import SearchFacetsView
 
@@ -8,4 +9,15 @@ class SearchView(SearchFacetsView):
     index = ViewPageTemplateFile('search.pt')
 
     def __call__(self):
+        b_start = 0
+        b_size = 30
+        catalog = getToolByName(self.context, 'portal_catalog')
+        self.results = catalog(
+            REQUEST=self.request,
+            use_types_blacklist=True,
+            use_navigation_root=True,
+            b_start=b_start,
+            b_size=b_size+1,
+            hl='true'
+        )
         return self.index()
